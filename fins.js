5c3737bc3dd371ced215102d88b34ad75f234587
Ludmylla28 <Ludmylla_thehands@hotmail.com>
2018-08-01 22:38:39 -0300

    testando 123

--- fins.js
+++
@@ -1,6 +0,0 @@
-var fim = '../testeschedule';
-var fimEnv = '../testeschedule/.env';
-var fimApp = '../testeschedule/app.js';
-var fimPackage = '../testeschedule/package.json';
-
-module.exports = { fim, fimEnv, fimApp, fimPackage };
--- schedule.js
+++ schedule.js
@@ -1,24 +1,57 @@
 var schedule = require('node-schedule');
 var shell = require('shelljs');
 var fs = require('file-system');
-var documentos = require('./documentos');
-var fins = require('./fins');
+
+var lista = [
+  '../../master/.vscode',
+  '../../master/componentes',
+  '../../master/dados',
+  '../../master/.vscode',
+  '../../master/componentes',
+  '../../master/dados',
+  '../../master/db',
+  '../../master/dialog',
+  '../../master/portal',
+  '../../master/public',
+  '../../master/security',
+  '../../master/server',
+  '../../master/service',
+  '../../master/social',
+  '../../master/utilities',
+  '../../master/validation',
+  '../../master/watson'];
+var env = '../../master/.env';
+var app = '../../master/app.js';
+var package = '../../master/package.json';
+
+var fim = '../testeschedule';
+var fimEnv = '../testeschedule/.env';
+var fimApp = '../testeschedule/app.js';
+var fimPackage = '../testeschedule/package.json';
 
 
+LLen = lista.length;
 
-LLen = documentos.length;
-
-var j = schedule.scheduleJob('29 * * * *', function () {
+var j = schedule.scheduleJob('38 * * * *', function () {
   for (i = 0; i < LLen; i++) {
-    shell.cp('-R', documentos[i], fins);
+    shell.cp('-R', lista[i], fim);
   }console.log('primeira etapa');
 
-  for (i = 0; i < documentos; i++){
-  fs.copyFile(env, fins, (err) => {
+  fs.copyFile(env, fimEnv, (err) => {
     if (err) throw err;
     console.log('.env not copied');
-  });
-}console.log('segunda etapa');
+  });console.log('segunda etapa');
+
+  fs.copyFile(app, fimApp, (err) => {
+    if (err) throw err;
+    console.log('.env not copied');
+  });console.log('segunda etapa');
+
+  fs.copyFile(package, fimPackage, (err) => {
+    if (err) throw err;
+    console.log('.env not copied');
+  });console.log('segunda etapa');
+
   if (shell.exec('cd ../testeschedule').code !== 0) {
     shell.echo('Error: folder change failed');
     shell.exit(1);
