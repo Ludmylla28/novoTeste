cec8d07e16446c31e3b38cf1f427812b44756588
Ludmylla28 <Ludmylla_thehands@hotmail.com>
2018-08-01 22:29:22 -0300

    testando 123

---
+++ fins.js
@@ -0,0 +1,6 @@
+var fim = '../testeschedule';
+var fimEnv = '../testeschedule/.env';
+var fimApp = '../testeschedule/app.js';
+var fimPackage = '../testeschedule/package.json';
+
+module.exports = { fim, fimEnv, fimApp, fimPackage };
--- schedule.js
+++ schedule.js
@@ -1,67 +1,36 @@
 var schedule = require('node-schedule');
 var shell = require('shelljs');
 var fs = require('file-system');
+var documentos = require('./documentos');
+var fins = require('./fins');
 
-var lista = [
-  '../../master/.vscode',
-  '../../master/componentes',
-  '../../master/dados',
-  '../../master/.vscode',
-  '../../master/componentes',
-  '../../master/dados',
-  '../../master/db',
-  '../../master/dialog',
-  '../../master/portal',
-  '../../master/public',
-  '../../master/security',
-  '../../master/server',
-  '../../master/service',
-  '../../master/social',
-  '../../master/utilities',
-  '../../master/validation',
-  '../../master/watson'];
 
-var fim = '../testeschedule';
 
-var env = '../../master/.env';
-var app = '../../master/app.js';
-var package = '../../master/package.json';
-var fimEnv = '../testeschedule/.env';
-var fimApp = '../testeschedule/app.js';
-var fimPackage = '../testeschedule/package.json';
+LLen = documentos.length;
 
-LLen = lista.length;
-
-var j = schedule.scheduleJob('04 * * * *', function () {
+var j = schedule.scheduleJob('29 * * * *', function () {
   for (i = 0; i < LLen; i++) {
-    shell.cp('-R', lista[i], fim);
-  }
-  fs.copyFile(env, fimEnv, (err) => {
+    shell.cp('-R', documentos[i], fins);
+  }console.log('primeira etapa');
+
+  for (i = 0; i < documentos; i++){
+  fs.copyFile(env, fins, (err) => {
     if (err) throw err;
     console.log('.env not copied');
   });
-
-  fs.copyFile(app, fimApp, (err) => {
-    if (err) throw err;
-    console.log('app.js not copied');
-  });
-
-  fs.copyFile(package, fimPackage, (err) => {
-    if (err) throw err;
-    console.log('package.json not copied');
-  });
+}console.log('segunda etapa');
   if (shell.exec('cd ../testeschedule').code !== 0) {
     shell.echo('Error: folder change failed');
-    shell.exit(1);console.log('folder change');
-  } else if (shell.exec('git add .').code !== 0) {
+    shell.exit(1);
+  }else if (shell.exec('git add .').code !== 0) {
     shell.echo('Error: Git add failed');
-    shell.exit(1);console.log('add done');
+    shell.exit(1);
   } else if (shell.exec('git commit -am "testando 123"').code !== 0) {
     shell.echo('Error: Git commit failed');
-    shell.exit(1);console.log('commit done');
+    shell.exit(1);
   } else if (shell.exec('git push').code !== 0) {
     shell.echo('Error: Git push failed');
-    shell.exit(1);console.log('push done');
+    shell.exit(1);
   }console.log('I finished')
 });
 
