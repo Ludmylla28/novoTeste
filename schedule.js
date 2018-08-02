7f5e6071d3e1fd0b9ee89766f35ac50e41e569e1
Ludmylla28 <Ludmylla_thehands@hotmail.com>
2018-08-01 21:47:49 -0300

    testando 123

--- schedule.js
+++ schedule.js
@@ -38,34 +38,34 @@ var j = schedule.scheduleJob('48 * * * *', function () {
   }
   fs.copyFile(env, fimEnv, (err) => {
     if (err) throw err;
-    console.log('.env copiado');
+    console.log('.env not copied');
   });
 
   fs.copyFile(app, fimApp, (err) => {
     if (err) throw err;
-    console.log('app.js copiado');
+    console.log('app.js not copied');
   });
 
   fs.copyFile(package, fimPackage, (err) => {
     if (err) throw err;
-    console.log('package.json copiado');
+    console.log('package.json not copied');
   })
 });
 if (j.code !== 0) {
   if (shell.exec('cd ../testeschedule').code !== 0) {
-    shell.echo('Error: Git commit failed');
-    shell.exit(1);
+    shell.echo('Error: folder change failed');
+    shell.exit(1);console.log('folder change');
   } else if (shell.exec('git add .').code !== 0) {
-    shell.echo('Error: Git commit failed');
-    shell.exit(1);
+    shell.echo('Error: Git add failed');
+    shell.exit(1);console.log('add done');
   } else if (shell.exec('git commit -am "testando 123"').code !== 0) {
     shell.echo('Error: Git commit failed');
-    shell.exit(1);
+    shell.exit(1);console.log('commit done');
   } else if (shell.exec('git push').code !== 0) {
-    shell.echo('Error: Git commit failed');
-    shell.exit(1);
+    shell.echo('Error: Git push failed');
+    shell.exit(1);console.log('push done');
   }
 }
-console.log('Terminei');
+console.log('I finished');
 
 module.exports = j; 
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const http = require('http')
 const ip = 'localhost'
 
 const server = http.createServer((req, res) => {
-  res.end('<h1>Testando o schedule</h1>')
+  res.end('<h1>Testing the schedule</h1>')
 })
 
 server.listen(port, ip, () => {
   j.runOnDate();
-  console.log(`Servidor rodando em http://${ip}:${port}`);
+  console.log(`Server running on http://${ip}:${port}`);
 });
